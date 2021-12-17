e55ded681f9ce7af5807948f5a138071c1ec42eb
nurmuhamadrum <nurmuhamadrum@gmail.com>
2021-12-17 15:59:43 +0700

    create CRUD method users

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var myLogger = function (req, res, next) {
 }
 
 app.use(myLogger)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(router)
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- routers.js
+++ routers.js
@@ -1,19 +1,109 @@
 const express = require('express')
+const { ObjectID } = require('mongodb')
 const router = express.Router()
 const connection = require('./connection')
 
+/**
+ * Get User method
+ */
 router.get('/users', async (req, res) => {
     try {
-        if(connection.connect()) {
+        if (connection.connect()) {
             const db = connection.db('db_latihan')
             const users = await db.collection('users').find().toArray()
 
-            res.send({ data: users})
+            res.send({ data: users })
         } else {
-            res.send({ message: 'connection database failed'})
+            res.send({ message: 'connection database failed' })
         }
     } catch (error) {
-        res.send({ message: error.message || 'internal server error'})
+        res.send({ message: error.message || 'internal server error' })
+    }
+})
+
+/**
+ * Add User method
+ */
+router.post('/users', async (req, res) => {
+    try {
+        if (connection.connect()) {
+            const { name, age, status } = req.body
+            const db = connection.db('db_latihan')
+
+            const user = await db.collection('users').insertOne({
+                name,
+                age,
+                status
+            })
+
+            if (user.insertedId) {
+                res.send({ message: 'successfuly add data' })
+            } else {
+                res.send({ message: 'failed to add data' })
+            }
+
+        } else {
+            res.send({ message: 'connection database failed' })
+        }
+    } catch (error) {
+        res.send({ message: error.message || 'internal server error' })
+    }
+})
+
+
+/**
+ * Updated User method
+ */
+router.put('/users/:id', async (req, res) => {
+    try {
+        if (connection.connect()) {
+            const { id } = req.params
+            const { name, age, status } = req.body
+            const db = connection.db('db_latihan')
+
+            const user = await db.collection('users').updateOne({ _id: ObjectID(id) }, {
+                $set: {
+                    name,
+                    age,
+                    status
+                }
+            })
+
+            if (user.modifiedCount === 1) {
+                res.send({ message: 'successfuly updated data' })
+            } else {
+                res.send({ message: 'failed updated data' })
+            }
+        } else {
+            res.send({ message: 'connection database failed' })
+        }
+    } catch (error) {
+        res.send({ message: error.message || 'internal server error' })
+    }
+})
+
+
+/**
+ * Delete User method
+ */
+router.delete('/users/:id', async (req, res) => {
+    try {
+        if (connection.connect()) {
+            const { id } = req.params
+            const db = connection.db('db_latihan')
+
+            const user = await db.collection('users').deleteOne({ _id: ObjectID(id) })
+
+            if (user.deletedCount === 1) {
+                res.send({ message: 'successfuly deleted data' })
+            } else {
+                res.send({ message: 'failed deleted data' })
+            }
+        } else {
+            res.send({ message: 'connection database failed' })
+        }
+    } catch (error) {
+        res.send({ message: error.message || 'internal server error' })
     }
 })
 
